94e63d019c50730c2aac910a25d1c658e35a7fd0
wepa <wepa.abdysetdarov@gmail.com>
2022-10-26 17:10:14 +0500

    App.js modified due to allow switch pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div className="main">
         <Header/>
-        
+        <div className="container">
+          <Routes>
+            <Route path = "/" element = {<Home/>}/>
+            <Route path = "/about" element = {<About/>}/>
+            <Route path = "/contact" element = {<Contact/>}/>
+          </Routes>
+        </div>
         <Footer/>
       </div>
       
