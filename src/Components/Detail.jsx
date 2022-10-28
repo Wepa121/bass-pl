123e53bd56f973e8d1bdfeb539083eefb936e54a
wepa <wepa.abdysetdarov@gmail.com>
2022-10-28 12:45:18 +0500

    modified Detail file to get related explanation

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom"
 import AsciiImage from "./AsciiImage"
+import { arr } from "../Data/data"
 
 function Detail() {
   const {component} = useParams()
   return (
     <div>
       <AsciiImage assetName={component}/>
-      <span>Yes! We all know what a switch is! Let's get to its technical definition. The primary purposes of a switch as a device are to break the current, interrupt the current, and supply current from one conductor to another. The “On and off” mechanism is used to perform this task. There are 4 main classifications of a switch
-      </span>
+      <span>{arr.filter((item)=> item.name.toLowerCase() === component)[0].explanation}</span>
     </div>
   )
 }
