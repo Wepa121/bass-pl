fecca793b628c01503e497729295e435e3f5e97c
wepa <wepa.abdysetdarov@gmail.com>
2022-10-28 12:28:07 +0500

    inserted AsciiImage to Detail file

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,9 +1,11 @@
-import { asciiInductor } from "./assets/ascii-art-inductor"
+import { useParams } from "react-router-dom"
+import AsciiImage from "./AsciiImage"
 
 function Detail() {
+  const {component} = useParams()
   return (
     <div>
-      <p className="asci item-ascii-art">{asciiInductor}</p>
+      <AsciiImage assetName={component}/>
       <span>Yes! We all know what a switch is! Let's get to its technical definition. The primary purposes of a switch as a device are to break the current, interrupt the current, and supply current from one conductor to another. The “On and off” mechanism is used to perform this task. There are 4 main classifications of a switch
       </span>
     </div>
