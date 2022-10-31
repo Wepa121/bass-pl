2cb8b811f7ca2f969a5ba6345cd6f7d6672842e5
wepa <wepa.abdysetdarov@gmail.com>
2022-10-31 10:16:24 +0500

    implement 404 page handler

--- src/Components/Detail.jsx
+++ src/Components/Detail.jsx
@@ -1,11 +1,18 @@
 import { useParams } from "react-router-dom"
 import AsciiImage from "./AsciiImage"
 import Explanation from "./Explanation"
+import {arr} from "../Data/data";
 
+const handlePageExist = (component) =>
+  arr.find(item => item.name.toLowerCase().includes(component.toLowerCase()))
 
 function Detail() {
   const {component} = useParams()
   
+  if (!handlePageExist(component)) {
+    return null;
+  }
+
   return (
     <div>
       <AsciiImage assetName={component}/>
